ce9953c364bf2841c1809dcfcc8810d24d94f129
prodbysky <kyrbynyt@gmail.com>
2024-04-28 13:02:17 +0300

    Move types to types.h

--- src/main.c
+++ src/main.c
@@ -4,15 +4,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long long u64;
-
-typedef signed char i8;
-typedef signed short i16;
-typedef signed int i32;
-typedef signed long long i64;
+#include "types.h"
 
 void resize_callback(GLFWwindow* window, int width, int height);
 GLFWwindow* init_window(u16 width, u16 height, const char* title);
