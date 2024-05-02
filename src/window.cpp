d5102c0ce16f401632680e033779adf849272ae0
prodbysky <kyrbynyt@gmail.com>
2024-05-02 16:23:18 +0300

    Move to GLAD for OpenGL 4.6 w/ all extensions (I don't know what will I need :))

--- src/window.cpp
+++ src/window.cpp
@@ -7,8 +7,8 @@
 
 Window::Window(u16 width, u16 height, const char* title) {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(width, height, title, NULL, NULL);
