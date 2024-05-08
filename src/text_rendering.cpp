826ce5f0d8eda6cc8263eef04e107423d8bc4cd3
prodbysky <kyrbynyt@gmail.com>
2024-05-08 15:47:06 +0300

    Update example

--- src/text_rendering.cpp
+++ src/text_rendering.cpp
@@ -3,25 +3,23 @@
 #include "window.h"
 
 int main() {
-    Window window(800, 800, "Hello world!");
+    Window window(800, 800, "Hello world!", false);
     window.fontSystem->LoadFont("assets/fonts/arial.ttf", 96);
     
     if (window.window == nullptr) {
         return -1;
     }
 
-    glm::vec2 pos = glm::vec2(300, 200);
-    float amplitude = 10.0f;
-    float frequency = 0.5f;
-    float time = 0.0f;
+    glm::vec2 pos = glm::vec2(300.0, 200.0);
+    f64 amplitude = 100.0;
+    f64 frequency = 5.0;
 
     while (!window.ShouldClose()) {
-        float yOffset = sin(time * frequency) * amplitude;
+        f64 yOffset = sin(window.GetTotalTime() * frequency) * amplitude * window.GetDeltaTime();
         pos.y += yOffset;
         window.Clear(24, 24, 24, 255);
         window.fontSystem->Draw("assets/fonts/arial.ttf", *window.shader, "2024 MUSIC", pos, 1, glm::vec3(1, 1, 1));
         window.Swap();
-        time += 0.1f;
     }
     return 0;
 }
