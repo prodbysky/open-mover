6204b085a9bdc520291f99a2600108e1cbd7b02d
prodbysky <kyrbynyt@gmail.com>
2024-05-09 20:04:44 +0300

    Type name consistency

--- src/assert.h
+++ src/assert.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "types.h"
+
 #define Assert(condition, message) Assert_(condition, message, __LINE__, __FILE__)
 
 namespace StintaEngine {
     // Assert function that calls `std::exit(0)` when `condition` is false
-    void Assert_(bool condition, const char* message, int line, const char* file);
+    void Assert_(bool condition, const char* message, i32 line, const char* file);
 }
