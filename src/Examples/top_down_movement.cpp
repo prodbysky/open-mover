81f351e25f1b25809c42c0dc962f91c2dafcfca1
prodbysky <kyrbynyt@gmail.com>
2024-05-11 18:05:47 +0300

    Add example :D

---
+++ src/Examples/top_down_movement.cpp
@@ -0,0 +1,43 @@
+#include "../Window/window.h"
+#include "../Shapes/textured_rect.h"
+#include "glad/glad.h"
+
+#define SPEED 300
+
+using namespace StintaEngine;
+
+void OrangeUpdate(Shapes::TexturedRect &orange, const Window& window);
+
+i32 main() {
+    Window window(800, 800, "Top-Down Movement", false); 
+    Core::TextureData orange_texture = window.resource_manager.LoadTexture("assets/sprites/orange.png");
+    Shapes::TexturedRect orange(glm::vec2(368, 432), 128, 128, orange_texture, GL_NEAREST, GL_RGBA8);
+
+    while (!window.ShouldClose()) {
+        OrangeUpdate(orange, window);
+        window.Clear(24, 24, 24, 255);
+        orange.Draw(*window.shader);
+        window.Swap();
+    }
+}
+
+void OrangeUpdate(Shapes::TexturedRect &orange, const Window& window) {
+    f32 speed = SPEED * window.GetDeltaTime();
+    if (window.input.KeyDown(GLFW_KEY_A)) {
+        orange.Move(glm::vec2(-speed, 0)); 
+    }
+    
+    if (window.input.KeyDown(GLFW_KEY_D)) {
+        orange.Move(glm::vec2(speed, 0)); 
+    }
+
+    
+    if (window.input.KeyDown(GLFW_KEY_W)) {
+        orange.Move(glm::vec2(0, speed)); 
+    }
+
+
+    if (window.input.KeyDown(GLFW_KEY_S)) {
+        orange.Move(glm::vec2(0, -speed)); 
+    }
+}
