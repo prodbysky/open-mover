1d8ff7f41576bfe454d4afc5f13fd09ca469168a
prodbysky <kyrbynyt@gmail.com>
2024-05-12 21:43:43 +0300

    Disable Vsync in rendering_textures

--- src/Examples/rendering_textures.cpp
+++ src/Examples/rendering_textures.cpp
@@ -2,7 +2,7 @@
 #include "../Window/window.h"
 
 int main() {
-    StintaEngine::Window window(800, 800, "Hello world!");
+    StintaEngine::Window window(800, 800, "Hello world!", false);
     
     const StintaEngine::Core::TextureData& orange = window.resource_manager.LoadTexture(std::string("assets/sprites/orange.png"));
     StintaEngine::Shapes::TexturedRect texture(glm::vec2(0, 800), 800, 800, orange, GL_NEAREST, GL_RGBA8);
