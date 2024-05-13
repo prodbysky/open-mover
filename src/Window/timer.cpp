39a7bb124796d3fbe98ff2f54239d4696093a99a
prodbysky <kyrbynyt@gmail.com>
2024-05-13 17:03:48 +0300

    Added a timer

---
+++ src/Window/timer.cpp
@@ -0,0 +1,21 @@
+#include "timer.h"
+
+namespace ZipLib {
+    Timer::Timer(f64 secs) {
+        duration = secs;
+        stopped  = true;
+    }
+
+    void Timer::Start() { stopped = false; }
+    void Timer::Stop() { stopped = true; }
+
+    void Timer::Extend(f64 secs) { duration += secs; }
+    void Timer::Trim(f64 secs) { duration -= secs; }
+
+    void Timer::Tick(f64 delta) {
+        if (!stopped && duration > 0)
+            duration -= delta;
+    }
+
+    bool Timer::Finished() { return duration <= 0; }
+} // namespace ZipLib
---
+++ src/Window/timer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../Utilities/utilities.h"
+
+namespace ZipLib {
+    class Timer {
+    private:
+        f64 duration;
+        bool stopped;
+
+    public:
+        Timer() = default;
+        Timer(f64 secs);
+
+        void Extend(f64 secs);
+        void Trim(f64 secs);
+
+        void Stop();
+        void Start();
+
+        void Tick(f64 delta);
+
+        bool Finished();
+    };
+} // namespace ZipLib
