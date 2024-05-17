2a8a25d0f0b542c4fd7fb99de799638478e11f5e
prodbysky <kyrbynyt@gmail.com>
2024-05-17 07:06:48 +0300

    Cleaned up animation implementation and usage (however didn't use timers this time)

--- src/Examples/animation_example.cpp
+++ src/Examples/animation_example.cpp
@@ -1,21 +1,18 @@
 #include "../Resource/animation.h"
-#include "../Shapes/textured_rect.h"
+#include "../Shapes/animated_rect.h"
 #include "../Window/window.h"
 #include "glad/glad.h"
 
 i32 main() {
     ZipLib::Window window(800, 800, "Hello world!");
-    ZipLib::Animation animation("assets/sprites/orange", 2, GL_MIRRORED_REPEAT,
-                                GL_NEAREST, GL_RGBA8, window.resource_manager);
-    ZipLib::Shapes::TexturedRect rect(glm::vec2(200, 400), 64, 64, {},
-                                      GL_NEAREST, GL_RGBA8);
-    rect.texture = animation.GetCurrentFrame();
+    ZipLib::Shapes::AnimatedRect rect(glm::vec2(200, 400), 64, 64,
+                                      "assets/sprites/orange", 2, GL_NEAREST,
+                                      GL_RGBA8, window.resource_manager);
 
     i32 i = 0;
     while (!window.ShouldClose()) {
         if (i % 60 == 0) {
-            animation.Advance();
-            rect.texture = animation.GetCurrentFrame();
+            rect.Advance();
         }
         window.Clear(24, 24, 24, 255);
         rect.Draw(*window.shader);
