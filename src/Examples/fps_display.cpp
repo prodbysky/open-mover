1feb8f682c4bec6fb15ea4fc9be9673e4e53c2c0
prodbysky <kyrbynyt@gmail.com>
2024-05-27 23:04:12 +0300

    Update the according examples

--- src/Examples/fps_display.cpp
+++ src/Examples/fps_display.cpp
@@ -8,7 +8,7 @@ int main() {
     const ZipLib::Font& font =
         window.resource_manager.LoadFont("assets/fonts/arial.ttf", 48);
     ZipLib::UI::Text fps_display(font, "", glm::vec2(400, 400),
-                                 glm::vec3(1, 1, 1), 1);
+                                 glm::vec3(1, 1, 1), 1, window.shader);
 
     u32 i = 0;
 
@@ -17,7 +17,7 @@ int main() {
         if (i % 10 == 0) {
             fps_display.SetText(std::to_string((u32) window.GetFPS()));
         }
-        fps_display.Draw(*window.shader);
+        fps_display.Draw();
         window.Swap();
         i++;
     }
--- src/Examples/text_rendering.cpp
+++ src/Examples/text_rendering.cpp
@@ -6,7 +6,7 @@ int main() {
     ZipLib::Font arial =
         window.resource_manager.LoadFont("assets/fonts/arial.ttf", 96);
     ZipLib::UI::Text text(arial, "2024 MUSIC", glm::vec2(300, 200),
-                          glm::vec3(1, 1, 1), 1);
+                          glm::vec3(1, 1, 1), 1, window.shader);
 
     f64 amplitude = 500.0;
     f64 frequency = 5.0;
@@ -16,7 +16,7 @@ int main() {
                        window.GetDeltaTime();
         text.SetPos(glm::vec2(200, 300 + y_offset));
         window.Clear(24, 24, 24, 255);
-        text.Draw(*window.shader);
+        text.Draw();
         window.Swap();
     }
     return 0;
